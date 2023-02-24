b1334374ed553c0666303766ecb7c2660b4109cf
XdHost <122685637+XdHost@users.noreply.github.com>
2023-02-24 18:58:45 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,11 +4,9 @@ set -e
 
 while true
 do
-  ffmpeg -loglevel info -y -re \
-    -f image2 -loop 1 -i bg.png \
-    -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
-    -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
-    -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
-    -c:a aac -b:a 128k -ar 44100 \
-    -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
+  ffmpeg \
+    -stream_loop -1 -re -i video.mp4 -stream_loop -1 -re -i https://stream-51.zeno.fm/8ymi1fecgqzvv?zs=ZPPD9y7OS8WJzwOqATMh8g  \
+    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+    -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
+    -f flv "$YOUTUBE_URL/$KEY
 done

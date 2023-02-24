0f65da97caff2b3e5e3f87d2610e08b5fbb875d4
XdHost <122685637+XdHost@users.noreply.github.com>
2023-02-24 19:03:29 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+7#!/bin/bash
 
 set -e
 
@@ -8,5 +8,5 @@ do
     -stream_loop -1 -re -i video.mp4 -stream_loop -1 -re -i https://stream-51.zeno.fm/8ymi1fecgqzvv?zs=ZPPD9y7OS8WJzwOqATMh8g  \
     -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
     -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
-    -f flv "$YOUTUBE_URL/$KEY
+    -f flv rtmp://x.rtmp.youtube.com/live2/$YOUTUBE_KEY
 done
